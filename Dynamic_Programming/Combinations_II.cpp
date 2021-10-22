82f058dcedbd7fae84dd53e3ca409535bce8b0e5
Amardeep Ganguly <83149058+punyprogrammer@users.noreply.github.com>
2021-10-22 13:33:36 +0530

    Create Combinations_II.cpp

---
+++ Dynamic_Programming/Combinations_II.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define mod 1000000007
+using namespace std;
+int main()
+{
+    int n;
+    ll x;
+    cin >> n >> x;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    vector<ll> dp(x + 1, 0);
+    dp[0] = 1;
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 1; i <= x; i++)
+        {
+            if (i >= a[j])
+            {
+                dp[i] = (dp[i] + dp[i - a[j]]) % mod;
+            }
+        }
+    }
+    cout << dp[x] << endl;
+}
